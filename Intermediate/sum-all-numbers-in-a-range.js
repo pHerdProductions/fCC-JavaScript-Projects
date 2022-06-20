c847cef9332cb7b5ab226bc1d5f290c8fd2a110e
Ralph <64453143+pHerdProductions@users.noreply.github.com>
2022-06-20 02:02:36 -0400

    Update sum-all-numbers-in-a-range.js

--- Intermediate/sum-all-numbers-in-a-range.js
+++ Intermediate/sum-all-numbers-in-a-range.js
@@ -1,11 +1,16 @@
-function sumAll(arr) { // Returns the total of all numbers including and between 2 integers
+// Returns the total of all numbers including and between 2 integers
+function sumAll(arr) {
+  
+  // Declare variables
   let sum = 0;
   const beg = Math.min(...arr); // The smaller of the two
   const end = Math.max(...arr); // The bigger of the two
+  
   // Iterate and add to sum
   for(let i = beg; i <= end; i++){
     sum += i;
   }
   
+  // Return our total sum
   return sum;
 }
